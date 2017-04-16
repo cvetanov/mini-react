fe4de755629824db161e0f0b57d9a79f7c223474
Goce Cvetanov <Goce.Cvetanov@netcetera.com>
2017-04-17 00:01:31 +0200

    add rollup bundle generation

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import path from 'path';
+import { rollup } from 'rollup';
+
+// generate bundle from main script and save it in 'static/bundle.js'
+rollup({
+  entry: 'src/main.js'
+}).then(bundle => {
+  bundle.write({
+    format: 'cjs',
+    dest: 'static/bundle.js'
+  });
+});
 
 const PORT = 8080;
 
 const app = express();
 
-app.use('/js', express.static('src'));
+// serve static files
+app.use('/static', express.static('static'));
+
 app.get('/home', (request, response) => {
   response.sendFile(path.join(`${__dirname}/index.html`));
 });
