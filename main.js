d155ebd4b3e96b3fa631ca9afd7a12ae4ad80ac3
Goce Cvetanov <Goce.Cvetanov@netcetera.com>
2017-04-16 22:19:26 +0200

    add babel transform react custom pragma

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { renderer } from './renderer';
+const demoJsx = <div id="first">Hi</div>;
+
 const increment = value => value + 1;
 
 export default increment;
---
+++ renderer.js
@@ -0,0 +1,5 @@
+const renderer = (...args) => { console.log(args); };
+
+export {
+  renderer
+};
